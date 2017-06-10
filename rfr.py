58aa04430119146a935c7c6eaa1a97ba69f85f3c
jjjjj@dcs02 <ytyteot51008@gmail.com>
2017-06-10 20:55:14 +0800

    Try shift feature.

--- rfr.py
+++ rfr.py
@@ -16,8 +16,19 @@ train_label_path = sys.argv[2]
 test_feature_path = sys.argv[3]
 submission_path = sys.argv[4]
 prediction_path = sys.argv[5]
+
 run_training = True
 run_testing = True
+WINDOW = 3
+MAX_EVAL = 500
+
+ADD_LAGGING = True
+LAGGING_WEEK = 2
+ADD_FULL_LAG = True
+EXPWA = False
+
+SHIFT_WEEK = True
+SHIFT_NUM = 2
 
 sj_train = []
 iq_train = []
@@ -38,9 +49,14 @@ predictor = ['week',
 			'station_max_temp_c',
 			'station_min_temp_c',
 			'station_precip_mm']
+core = ['reanalysis_specific_humidity_g_per_kg', 
+		'reanalysis_dew_point_temp_k', 
+		'station_avg_temp_c', 
+		'station_min_temp_c']
 target = 'total_cases'
 min_error = np.inf
-WINDOW = 3
+lag_predictor = []
+pred = []
 
 def preprocess_data(data_path, labels_path=None):
 	print('Reading data from:', data_path)
@@ -58,21 +74,76 @@ def preprocess_data(data_path, labels_path=None):
 	if labels_path:
 		labels = pd.read_csv(labels_path, index_col=[0, 1, 2])
 		df = df.join(labels)
-
+	
 	# separate san juan and iquitos
 	sj = df.loc['sj']
 	iq = df.loc['iq']
 
 	return (sj, iq)
 
+def set_predictor(lagging=ADD_LAGGING):
+	global pred
+	if lagging:
+		pred = lag_predictor
+		print('Set lagging predictors.')
+	else:
+		pred = predictor
+		print('Set normal predictors.')
+	# print(pred)
+
+def add_lagging_data(df, lag_num=LAGGING_WEEK, full_data=ADD_FULL_LAG, expwa=EXPWA):
+	global lag_predictor
+	lag_predictor = list(predictor)
+	# decide lagging data tp be added
+	if ADD_FULL_LAG:
+		features = list(predictor)
+		print('Add full lagging data:', LAGGING_WEEK, 'week(s)')
+	else:
+		features = list(core)
+		print('Add core lagging data:', LAGGING_WEEK, 'week(s)')
+
+	# add lagging data
+	for feature in features:
+		for i in range(lag_num):
+			lagging_feature = feature + '_lag' + str(i+1)
+			df[lagging_feature] = df[feature].shift(i+1)
+			if lagging_feature not in lag_predictor:
+				lag_predictor.append(lagging_feature)
+
+	# fill nan in first lag_num rows (not having lagging data)
+	df.fillna(method='bfill', inplace=True)
+	# df.fillna(df.mean(), inplace=True)
+
+	return df
+
+def shift_week_training(df, shift_week_num=SHIFT_NUM):
+	print('Training data shift:', shift_week_num, 'week(s)')
+	df['total_cases'] = df['total_cases'].shift(-1*shift_week_num)
+	return df.iloc[:-1*shift_week_num]
+
+def shift_week_testing(df, shift_week_num=SHIFT_NUM):
+	print('Testing data shift:', shift_week_num, 'week(s)')
+	# if not run_training:
+	# 	global sj_train, iq_train
+	# 	sj_train, iq_train = preprocess_data(train_feature_path, labels_path=train_label_path)
+	df = df.shift(shift_week_num)
+	df.fillna(method='bfill', inplace=True)
+
+	return df
+
+def add_monthly_mean():
+	return None
+
+def add_rolling_mean():
+	return None
+
 def moving_avg(data, n=3):
 	ret = np.cumsum(data)
-	ret[n:] = ret[n::] - ret[:-n]
+	ret[n::] = ret[n::] - ret[:-n]
 	ret = np.hstack((ret[:n-1],ret[n-1::]/n))
 	return ret
 
 def rolling_cross_validation(data, model, cv_split=5, window_size=3):
-	global predictor, target
 	tscv = TimeSeriesSplit(n_splits=cv_split)
 	err = []
 
@@ -80,8 +151,8 @@ def rolling_cross_validation(data, model, cv_split=5, window_size=3):
 		train = data.iloc[train_idx]
 		test = data.iloc[test_idx]
 
-		model.fit(train[predictor], train[target])
-		result = model.predict(test[predictor])
+		model.fit(train[pred], train[target])
+		result = model.predict(test[pred])
 		result = moving_avg(result, n=window_size)
 
 		e = mean_absolute_error(np.round(result), test[target])
@@ -113,16 +184,28 @@ def hyperopt_objective(params):
 
 def optimize_model(trials):
 	print('Optimizing model for city:', TRAIN_CITY)
-	param_space = {'max_depth': hp.quniform('max_depth', 1, 300, 1), # 1-200
-					'max_features': hp.quniform('max_features', 1, len(predictor), 1),
-					'n_estimators': hp.quniform('n_estimators', 300, 750, 1), # 250-750
+	param_space = {'max_depth': hp.quniform('max_depth', 1, 200, 1), # 1-200
+					'max_features': hp.quniform('max_features', 1, len(pred), 1),
+					'n_estimators': hp.quniform('n_estimators', 250, 750, 1), # 250-750
 					'min_samples_split': hp.quniform('min_samples_split', 2, 100, 1),
-					'min_samples_leaf': hp.quniform('min_samples_leaf', 1, 10, 1)}
+					'min_samples_leaf': hp.quniform('min_samples_leaf', 1, 100, 1)}
 
-	best_params = fmin(hyperopt_objective, param_space, algo=tpe.suggest, trials=trials, max_evals=1000)
+	best_params = fmin(hyperopt_objective, param_space, algo=tpe.suggest, trials=trials, max_evals=MAX_EVAL)
 
 	print('\nBest parameters:', best_params, '\n')
 	return best_params
+
+def get_best_model(sj_params, iq_params):
+	print('Training best models.')
+	sj_model = RandomForestRegressor(**sj_params)
+	sj_model.fit(sj_train[pred], sj_train[target])
+	iq_model = RandomForestRegressor(**iq_params)
+	iq_model.fit(iq_train[pred], iq_train[target])
+
+	with open('./rfr_models/sj_rfr_model.pickle','wb') as f:
+		pickle.dump(sj_model, f)
+	with open('./rfr_models/iq_rfr_model.pickle','wb') as f:
+		pickle.dump(iq_model, f)
 
 def training(data_path, labels_path):
 	print('Start training...')
@@ -131,45 +214,71 @@ def training(data_path, labels_path):
 	global sj_train, iq_train
 	sj_train, iq_train = preprocess_data(data_path, labels_path=labels_path)
 
+	### Shift feature week
+	if SHIFT_WEEK:
+		sj_train = shift_week_training(sj_train)
+		iq_train = shift_week_training(iq_train)
+
+	### Add lagging data
+	if ADD_LAGGING:
+		sj_train = add_lagging_data(sj_train)
+		iq_train = add_lagging_data(iq_train)
+
+	### Shuffle
+	# sj_train = sj_train.sample(frac=1)
+	# iq_train = iq_train.sample(frac=1)
+
+	### Set predictor
+	set_predictor()
+
 	### Training
 	global min_error, TRAIN_CITY
 	TRAIN_CITY = 'sj'
 	sj_trials = Trials()
 	sj_params = optimize_model(sj_trials)
 	sj_params = {arg: int(value) for arg, value in sj_params.items() if type(value)==type(float(0))}
+	sj_best = min_error
 
 	TRAIN_CITY = 'iq'
 	min_error = np.inf
 	iq_trials = Trials()
 	iq_params = optimize_model(iq_trials)
 	iq_params = {arg: int(value) for arg, value in iq_params.items() if type(value)==type(float(0))}
+	iq_best = min_error
+
+	print('MAE:', (sj_best*936 + iq_best*520) / 1456, '\n')
 
 	### Get best model
-	sj_model = RandomForestRegressor(**sj_params)
-	sj_model.fit(sj_train[predictor], sj_train[target])
-	iq_model = RandomForestRegressor(**iq_params)
-	iq_model.fit(iq_train[predictor], iq_train[target])
-
-	with open('sj_rfr_model.pickle','wb') as f:
-		pickle.dump(sj_model, f)
-	with open('iq_rfr_model.pickle','wb') as f:
-		pickle.dump(iq_model, f)
+	get_best_model(sj_params, iq_params)
 
 	print('Training is done.')
 
 def testing(data_path, sub_path, pred_path, sj_model_path, iq_model_path):
 	print('Start testing...')
 
-	global predictor
 	sj_test, iq_test = preprocess_data(data_path)
+
+	### Shift feature week
+	if SHIFT_WEEK:
+		sj_test = shift_week_testing(sj_test)
+		iq_test = shift_week_testing(iq_test)
+
+	### Add lagging data
+	if ADD_LAGGING:
+		sj_test = add_lagging_data(sj_test)
+		iq_test = add_lagging_data(iq_test)
+		
+	set_predictor()
 
 	with open(sj_model_path,'rb') as f:
 		sj_model = pickle.load(f)
+	print('Loading sj model from: ', sj_model_path)
 	with open(iq_model_path,'rb') as f:
 		iq_model = pickle.load(f)
+	print('Loading iq model from: ', iq_model_path)
 
-	sj_predictions = sj_model.predict(sj_test[predictor]).astype(float)
-	iq_predictions = iq_model.predict(iq_test[predictor]).astype(float)
+	sj_predictions = sj_model.predict(sj_test[pred]).astype(float)
+	iq_predictions = iq_model.predict(iq_test[pred]).astype(float)
 
 	print('Moving average window size:', WINDOW)
 	sj_predictions = np.round(moving_avg(sj_predictions, n=WINDOW)).astype(int)
@@ -191,8 +300,8 @@ def main():
 
 	### Testing
 	if run_testing:
-		sj_model_path = 'sj_rfr_model.pickle'
-		iq_model_path = 'iq_rfr_model.pickle'
+		sj_model_path = './rfr_models/sj_rfr_model.pickle'
+		iq_model_path = './rfr_models/iq_rfr_model.pickle'
 		testing(test_feature_path, submission_path, prediction_path, sj_model_path, iq_model_path)
 
 if __name__=='__main__':
--- run_rfr.sh
+++ run_rfr.sh
@@ -3,6 +3,6 @@ TRAIN_F=./data/train_feature.csv
 TRAIN_L=./data/train_label.csv
 TEST_F=./data/test_feature.csv
 SUB=./data/submission_format.csv
-PRED=./prediction_rfr.csv
+PRED=./pred_rfr.csv
 
 python3.6 ./rfr.py $TRAIN_F $TRAIN_L $TEST_F $SUB $PRED 
