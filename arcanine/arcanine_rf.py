d37aa6501aebdbdb080a7af30f4a80b3ec471ad2
Arcanine901097 <b02901097@ntu.edu.tw>
2017-06-12 17:58:20 +0800

    Add files via upload

--- arcanine/arcanine_rf.py
+++ arcanine/arcanine_rf.py
@@ -13,9 +13,7 @@ features = ['ndvi_ne', 'ndvi_nw', 'ndvi_se', 'ndvi_sw', 'precipitation_amt_mm',
        'station_avg_temp_c', 'station_diur_temp_rng_c', 'station_max_temp_c',
 	   'station_min_temp_c', 'station_precip_mm']
 
-add_features = ['reanalysis_dew_point_temp_k', 'reanalysis_min_air_temp_k', 
-	'reanalysis_specific_humidity_g_per_kg', 'station_min_temp_c',
-	'station_avg_temp_c']
+add_features = ['reanalysis_specific_humidity_g_per_kg', 'station_min_temp_c', 'station_avg_temp_c']
 
 # Read training & validation & testing data
 df_train = pd.read_csv('train_feature.csv',encoding="big5")
@@ -57,25 +55,71 @@ for i,feature in enumerate(line):
 
 for i,feature in enumerate(line):
 	iq_test_dict[feature] = iq_test[:,i+4].reshape((len(iq_test[:,i+4]),1))
-
+"""
 # Add new feature
 sj_train_feature = sj_train[:,4:]
 sj_test_feature = sj_test[:,4:]
 iq_train_feature = iq_train[:,4:]
 iq_test_feature = iq_test[:,4:]
-"""
-for feature in add_features:
-	sj_train_feature = np.hstack((sj_train_feature,sj_train_dict[feature]**2))
 
 for feature in add_features:
-	sj_test_feature = np.hstack((sj_test_feature,sj_test_dict[feature]**2))
+	sj_train_feature = np.hstack((sj_train_feature,sj_train_dict[feature]**3))
+
+for feature in add_features:
+	sj_test_feature = np.hstack((sj_test_feature,sj_test_dict[feature]**3))
 	
 for feature in add_features:
-	iq_train_feature = np.hstack((iq_train_feature,iq_train_dict[feature]**2))
+	iq_train_feature = np.hstack((iq_train_feature,iq_train_dict[feature]**3))
 	
 for feature in add_features:
-	iq_test_feature = np.hstack((iq_test_feature,iq_test_dict[feature]**2))
+	iq_test_feature = np.hstack((iq_test_feature,iq_test_dict[feature]**3))
 """	
+# Choose feature
+sj_train_feature = sj_train_dict['reanalysis_dew_point_temp_k']
+sj_test_feature = sj_test_dict['reanalysis_dew_point_temp_k']
+iq_train_feature = iq_train_dict['reanalysis_dew_point_temp_k']
+iq_test_feature = iq_test_dict['reanalysis_dew_point_temp_k']
+
+for feature in add_features:
+	sj_train_feature = np.hstack((sj_train_feature,sj_train_dict[feature]))
+	sj_test_feature = np.hstack((sj_test_feature,sj_test_dict[feature]))
+	iq_train_feature = np.hstack((iq_train_feature,iq_train_dict[feature]))
+	iq_test_feature = np.hstack((iq_test_feature,iq_test_dict[feature]))
+
+sj_train_feature = np.hstack((sj_train_feature,sj_train_dict['reanalysis_dew_point_temp_k']**3))
+sj_test_feature = np.hstack((sj_test_feature,sj_test_dict['reanalysis_dew_point_temp_k']**3))
+iq_train_feature = np.hstack((iq_train_feature,iq_train_dict['reanalysis_dew_point_temp_k']**3))
+iq_test_feature = np.hstack((iq_test_feature,iq_test_dict['reanalysis_dew_point_temp_k']**3))
+	
+for feature in add_features:
+	sj_train_feature = np.hstack((sj_train_feature,sj_train_dict[feature]**3))
+	sj_test_feature = np.hstack((sj_test_feature,sj_test_dict[feature]**3))
+	iq_train_feature = np.hstack((iq_train_feature,iq_train_dict[feature]**3))
+	iq_test_feature = np.hstack((iq_test_feature,iq_test_dict[feature]**3))
+
+# Add ten weeks feature
+sj_train_tmp = np.zeros((len(sj_train_feature)-10,90))
+iq_train_tmp = np.zeros((len(iq_train_feature)-10,90))
+#sj_train_tmp = sj_train_feature[9:,:]
+#iq_train_tmp = iq_train_feature[9:,:]
+
+for i in range(len(sj_train_tmp)):
+	tmp = sj_train_feature[i+9]
+	for j in range(i+8, i-1, -1):
+		tmp = np.hstack((tmp,sj_train_feature[j]))
+	tmp = np.hstack((tmp,sj_label[:,3][i:i+10]))
+	sj_train_tmp[i] = tmp
+
+for i in range(len(iq_train_tmp)):
+	tmp = iq_train_feature[i+9]
+	for j in range(i+8, i-1, -1):
+		tmp = np.hstack((tmp,iq_train_feature[j]))
+	tmp = np.hstack((tmp,iq_label[:,3][i:i+10]))
+	iq_train_tmp[i] = tmp
+	
+sj_train_feature = sj_train_tmp
+iq_train_feature = iq_train_tmp
+	
 # Normalize
 sj_train_mean = np.mean(sj_train_feature, axis=0)
 sj_train_std = np.std(sj_train_feature.astype('float'), axis=0)
@@ -86,8 +130,8 @@ iq_train_std = np.std(iq_train_feature.astype('float'), axis=0)
 iq_train_feature = (iq_train_feature - iq_train_mean) / iq_train_std
 
 # Split into train, val dataset
-sj_train, sj_val, sj_train_label, sj_val_label = cross_validation.train_test_split(sj_train_feature, sj_label[:,3])
-iq_train, iq_val, iq_train_label, iq_val_label = cross_validation.train_test_split(iq_train_feature, iq_label[:,3]) 
+sj_train, sj_val, sj_train_label, sj_val_label = cross_validation.train_test_split(sj_train_feature, sj_label[:,3][10:])
+iq_train, iq_val, iq_train_label, iq_val_label = cross_validation.train_test_split(iq_train_feature, iq_label[:,3][10:]) 
 
 # Random Forest Regression
 regressor_sj = RandomForestRegressor(n_estimators=250, min_samples_split=2)
@@ -100,9 +144,9 @@ sj_train_result = regressor_sj.predict(sj_train)
 sj_val_result = regressor_sj.predict(sj_val)
 
 for i,j in enumerate(sj_train_result):
-	sj_train_result[i] = round(j)
+	sj_train_result[i] = int(round(j))
 for i,j in enumerate(sj_val_result):
-	sj_val_result[i] = round(j)
+	sj_val_result[i] = int(round(j))
 	
 loss_train = metrics.mean_absolute_error(sj_train_result, sj_train_label)
 loss_val = metrics.mean_absolute_error(sj_val_result, sj_val_label)
@@ -113,15 +157,15 @@ iq_train_result = regressor_iq.predict(iq_train)
 iq_val_result = regressor_iq.predict(iq_val)
 
 for i,j in enumerate(iq_train_result):
-	iq_train_result[i] = round(j)
+	iq_train_result[i] = int(round(j))
 for i,j in enumerate(iq_val_result):
-	iq_val_result[i] = round(j)
+	iq_val_result[i] = int(round(j))
 	
 loss_train = metrics.mean_absolute_error(iq_train_result, iq_train_label)
 loss_val = metrics.mean_absolute_error(iq_val_result, iq_val_label)
 print('iq Training loss = ' + str(loss_train))
 print('iq Validation loss = ' + str(loss_val))
-
+"""
 # Predict & Output
 sj_test_feature = (sj_test_feature - sj_train_mean) / sj_train_std
 iq_test_feature = (iq_test_feature - iq_train_mean) / iq_train_std
@@ -135,9 +179,10 @@ for i,row in enumerate(pred_sj):
 	string = ''
 	for j in sj_test[i][:3]:
 		string += (str(j) + ',')
-	file_write.write(string + str(round(row)) + '\n')
+	file_write.write(string + str(int(round(row))) + '\n')
 for i,row in enumerate(pred_iq):
 	string = ''
 	for j in iq_test[i][:3]:
 		string += (str(j) + ',')
-	file_write.write(string + str(round(row)) + '\n')
+	file_write.write(string + str(int(round(row))) + '\n')
+"""
